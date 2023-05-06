7a30a4cd211cbd6bc76942b9667309d6aff79ece
DavidAnacona <danaconac@ucentral.edu.co>
2023-05-06 14:25:27 -0500

    login y estilos inicales

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Box, Container, Typography, TextField, Button } from '@mui/material'
+import back_log from "../../back-log.jpg"
+
+const login = () => {
+  return (
+    <>
+        <Box sx={{ 
+        width: "100vw", 
+        height: "100vh", 
+        backgroundImage: `url('${back_log}')`,
+        backgroundPosition: 'center center',
+        backgroundSize: "cover",
+        opacity: "0.6",
+        position: "relative"
+        }}>
+
+        </Box>
+        <Container sx={{
+            position: "absolute",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            opacity: "1",
+            width: "500px", 
+            height: "500px", 
+            backgroundColor: "white",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "20px",
+            flexDirection: "column",
+            fontSize: "32px"
+            }}>
+            <Typography variant='h3' sx={{fontWeight: "bold", fontFamily: "monospace"}}>Bienvenido a CPDC</Typography>
+            <TextField fullWidth inputProps={{ style: { fontSize: "22px" } }}  id="standard-basic" label="Usuario" variant="standard" size='medium' sx={{marginTop: "40px", fontFamily: "monospace"}}/>
+            <TextField fullWidth inputProps={{ style: { fontSize: "22px" } }} fontWeight="bold" id="standard-basic" label="Contraseña" variant="standard" size='medium' sx={{marginTop: "40px", fontFamily: "monospace"}}/>
+            <Button variant="contained" sx={{fontSize: "22px", width: "300px", marginTop: "60px", fontFamily: "monospace", fontWeight: "bold"}}>Iniciar sesión</Button>
+        </Container>
+    </>
+    
+  )
+}
+
+export default login
